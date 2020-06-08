65837ee8d417dc4b0452e42f8f30ec6b3fefd4aa
adamhutchings <55220104+adamhutchings@users.noreply.github.com>
2020-06-08 15:18:00 -0700

    Turn int to float

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ goodnesses = []
 with open('ratings.txt') as ratings:
   for line in ratings:
     pair = line.split(' ')
-    lawfulnesses.append(int(pair[0]))
-    goodnesses.append(int(pair[1]))
+    lawfulnesses.append(float(pair[0]))
+    goodnesses.append(float(pair[1]))
     
 plt.xlabel('Lawfulness')
 plt.ylabel('Goodness')
